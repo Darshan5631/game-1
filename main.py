8ac7f026ebc2b08b355dded71cb3aefd3067b039
DARSHAN S <101651363+Darshan5631@users.noreply.github.com>
2022-08-01 14:43:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+a=input('where do u wanna go "left" or "right"').lower()
+if a == "right": 
+  print("Fell into a hole;Game Over." )
+else :
+  b=input("do u wanna swin or wait").lower()
+  if b == "swim":
+    print("attacked by shark game over")
+  elif b=="wait" :
+    c=input('which door u want "red" or "blue" or "yellow"').lower()
+    if c == "red":
+      print("burned by fire game over")
+    elif c == "blue":
+      print("eaten by beast game over")
+    elif c == "yellow":
+      print("you win")
+    else:
+      print("bad luck no door exists game over")
+  else:
+    print("bad luck")
